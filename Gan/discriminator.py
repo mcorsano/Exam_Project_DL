46c35913a97f80db148c30baaada08aeaf710683
mcorsano <mariannacorsano98@gmail.com>
2022-09-25 13:05:49 +0200

    gan discriminator minor fixes

--- Gan/discriminator.py
+++ Gan/discriminator.py
@@ -4,13 +4,13 @@ import torch.nn as nn
 
 
 class Discriminator(nn.Module):
-    def __init__(self, in_features):
+    def __init__(self, img_dim):
         super().__init__()
         self.mod = nn.Sequential(
-            nn.Linear(in_features, 128),
+            nn.Linear(img_dim, 128),
             nn.LeakyReLU(0.01),
             nn.Linear(128, 1),   # fake=0, real=1
-            nn.Sigmoid(),   
+            nn.Sigmoid() 
         )
 
     def forward(self, x):
